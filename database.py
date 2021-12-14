6da2d89ac62c0cd35b8ab53a14b00ae42d89b5f7
asgutierrez <asgutierrez@uninorte.edu.co>
2021-12-13 21:21:49 -0500

    postgres

--- database.py
+++ database.py
@@ -7,4 +7,7 @@ URL_DB = 'localhost'
 NAME_DB = 'launch_mobility'
 FULL_URL_DB = os.environ.get('DATABASE_URL', f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}')
 
+if FULL_URL_DB and FULL_URL_DB.startswith("postgres://"):
+    FULL_URL_DB = FULL_URL_DB.replace("postgres://", "postgresql://", 1)
+
 db = SQLAlchemy()
